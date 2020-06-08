b19ad11c9453ae99b117c26c7ccf74f7cf5604d2
atnamin <a.amir.namin@gmail.com>
2020-06-08 09:14:33 -0400

    taking off tensorflor

--- dft01_app.py
+++ dft01_app.py
@@ -173,7 +173,8 @@ def load_prediction_models(model_file):
 	loaded_model = joblib.load(open(os.path.join(model_file),"rb"))
 	return loaded_model
 
-import keras.backend.tensorflow_backend as tb
+#import keras.backend.tensorflow_backend as tb
+import keras.backend as tb
 tb._SYMBOLIC_SCOPE.value = True
 
 if choices == 'Prediction':
